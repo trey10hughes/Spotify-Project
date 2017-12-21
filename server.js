9935f636fe9d61ef3b246c8281c743e55c055c89
Theodore hughes <theodorehughes@Theodores-MacBook-Pro.local>
2017-12-20 20:59:33 -0600

    back end is working, starting point for front end

---
+++ server.js
@@ -0,0 +1,23 @@
+var express = require("express");
+var path = require("path");
+var bodyParser = require("body-parser");
+var request = require("request");
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+app.use(express.static("views/assets"));
+
+
+
+var PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("App listening on Port " + PORT);
+});
+
+
+
+require("./authorization_code/app.js")(app, path, bodyParser);
