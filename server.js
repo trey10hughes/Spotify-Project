eb2203a5e0d05e054a9fa0df3bd9e5c340cd8abb
Theodore hughes <theodorehughes@Theodores-MacBook-Pro.local>
2018-01-06 11:39:10 -0600

    basic functionality complete

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
-app.use(express.static("views/assets"));
+app.use(express.static("authorization_code/public"));
 
 
 
